8b3d607af5a1fc47f11eea9a1d132c59852f8f9d
宇鹰 <liaojinwei.ljw@alibaba-inc.com>
2021-10-31 21:33:16 +0800

    format

--- app/BinanceAPI.py
+++ app/BinanceAPI.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*
 import requests, time, hmac, hashlib
-from app.authorization import recv_window,api_secret,api_key
+from app.authorization import recv_window, api_secret, api_key
 
 try:
     from urllib import urlencode
 # python3
 except ImportError:
     from urllib.parse import urlencode
+
 
 class BinanceAPI(object):
     BASE_URL = "https://www.binance.com/api/v1"
@@ -21,26 +22,26 @@ class BinanceAPI(object):
         path = "%s/ping" % self.BASE_URL_V3
         return requests.get(path, timeout=180, verify=True).json()
 
-    def get_ticker_price(self,market):
+    def get_ticker_price(self, market):
         path = "%s/ticker/price" % self.BASE_URL_V3
-        params = {"symbol":market}
-        res =  self._get_no_sign(path,params)
+        params = {"symbol": market}
+        res = self._get_no_sign(path, params)
         time.sleep(2)
         return float(res['price'])
 
-    def get_ticker_24hour(self,market):
+    def get_ticker_24hour(self, market):
         path = "%s/ticker/24hr" % self.BASE_URL_V3
-        params = {"symbol":market}
-        res =  self._get_no_sign(path,params)
+        params = {"symbol": market}
+        res = self._get_no_sign(path, params)
         return res
 
     def get_klines(self, market, interval, startTime=None, endTime=None):
         path = "%s/klines" % self.BASE_URL_V3
         params = None
         if startTime is None:
-            params = {"symbol": market, "interval":interval}    
-        else:    
-            params = {"symbol": market, "interval":interval, "startTime":startTime, "endTime":endTime}
+            params = {"symbol": market, "interval": interval}
+        else:
+            params = {"symbol": market, "interval": interval, "startTime": startTime, "endTime": endTime}
         return self._get_no_sign(path, params)
 
     def buy_limit(self, market, quantity, rate):
@@ -99,14 +100,14 @@ class BinanceAPI(object):
         query = urlencode(self._sign(params))
         url = "%s" % (path)
         header = {"X-MBX-APIKEY": self.key}
-        return requests.post(url, headers=header, data=query, \
-            timeout=180, verify=True).json()
+        return requests.post(url, headers=header, data=query, timeout=180, verify=True).json()
 
     def _format(self, price):
         return "{:.8f}".format(price)
 
+
 if __name__ == "__main__":
-    instance = BinanceAPI(api_key,api_secret)
+    instance = BinanceAPI(api_key, api_secret)
     # print(instance.buy_limit("EOSUSDT",5,2))
     # print(instance.get_ticker_price("WINGUSDT"))
     print(instance.get_ticker_24hour("WINGUSDT"))
--- app/dingding.py
+++ app/dingding.py
@@ -1,26 +1,27 @@
-import requests,json
+import requests, json
 
 # windows
-from app.authorization import dingding_token, recv_window,api_secret,api_key
+from app.authorization import dingding_token, recv_window, api_secret, api_key
 from app.BinanceAPI import BinanceAPI
+
+
 # linux
 # from app.authorization import dingding_token
 
 class Message:
 
-    def buy_limit_msg(self,market, quantity, rate):
+    def buy_limit_msg(self, market, quantity, rate):
         try:
-            res = BinanceAPI(api_key,api_secret).buy_limit(market, quantity, rate)
+            res = BinanceAPI(api_key, api_secret).buy_limit(market, quantity, rate)
             if res['orderId']:
-                buy_info = "报警：币种为：{cointype}。买单价为：{price}。买单量为：{num}".format(cointype=market,price=rate,num=quantity)
+                buy_info = "报警：币种为：{cointype}。买单价为：{price}。买单量为：{num}".format(cointype=market, price=rate, num=quantity)
                 self.dingding_warn(buy_info)
                 return res
         except BaseException as e:
-            error_info = "报警：币种为：{cointype},买单失败.api返回内容为:{reject}".format(cointype=market,reject=res['msg'])
+            error_info = "报警：币种为：{cointype},买单失败.api返回内容为:{reject}".format(cointype=market, reject=res['msg'])
             self.dingding_warn(error_info)
 
-
-    def sell_limit_msg(self,market, quantity, rate):
+    def sell_limit_msg(self, market, quantity, rate):
         '''
         :param market:
         :param quantity: 数量
@@ -28,23 +29,23 @@ class Message:
         :return:
         '''
         try:
-            res = BinanceAPI(api_key,api_secret).sell_limit(market, quantity, rate)
+            res = BinanceAPI(api_key, api_secret).sell_limit(market, quantity, rate)
             if res['orderId']:
-                buy_info = "报警：币种为：{cointype}。卖单价为：{price}。卖单量为：{num}".format(cointype=market,price=rate,num=quantity)
+                buy_info = "报警：币种为：{cointype}。卖单价为：{price}。卖单量为：{num}".format(cointype=market, price=rate, num=quantity)
                 self.dingding_warn(buy_info)
                 return res
         except BaseException as e:
-            error_info = "报警：币种为：{cointype},卖单失败.api返回内容为:{reject}".format(cointype=market,reject=res['msg'])
-            self.dingding_warn(error_info+str(res))
+            error_info = "报警：币种为：{cointype},卖单失败.api返回内容为:{reject}".format(cointype=market, reject=res['msg'])
+            self.dingding_warn(error_info + str(res))
             return res
 
-    def dingding_warn(self,text):
+    def dingding_warn(self, text):
         headers = {'Content-Type': 'application/json;charset=utf-8'}
         api_url = "https://oapi.dingtalk.com/robot/send?access_token=%s" % dingding_token
         json_text = self._msg(text)
         requests.post(api_url, json.dumps(json_text), headers=headers).content
 
-    def _msg(self,text):
+    def _msg(self, text):
         json_text = {
             "msgtype": "text",
             "at": {
@@ -59,6 +60,7 @@ class Message:
         }
         return json_text
 
+
 if __name__ == "__main__":
     msg = Message()
-    print(msg.buy_limit_msg("EOSUSDT",4,2))
+    print(msg.buy_limit_msg("EOSUSDT", 4, 2))
